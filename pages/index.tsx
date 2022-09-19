0ca1c33306a429a3e0dd48fe90b1dbe9dce05a32
Adetunji Samuel <samadetunji01@gmail.com>
2022-09-19 19:34:43 +0100

    chore: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
